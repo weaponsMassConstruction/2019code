5b08297d665996834b3442223203e0a7cdf42e5c
Chrisvin123 <christophervin1@gmail.com>
2019-02-10 12:11:09 -0500

    WIP - Closed loop arm control

--- src/main/java/frc/robot/Arm.java
+++ src/main/java/frc/robot/Arm.java
@@ -1,10 +1,15 @@
-public class Arm{
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
+public class Arm {
     WoMC ob;
 
-    public Arm(WoMC o){
-        ob=o;
+    public Arm(WoMC o) {
+        ob = o;
     }
-    public void setPosition(double position){
-        
+
+    public void setPosition(double position) {
+        ob.arm.set(ControlMode.Position, position);
     }
 }
--- src/main/java/frc/robot/WoMC.java
+++ src/main/java/frc/robot/WoMC.java
@@ -1,3 +1,4 @@
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
@@ -18,11 +19,13 @@ public class WoMC{
         rightFront = new PWM(4);
         //Arm
         arm = new TalonSRX(1);
+        arm.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute);
+        arm.config_kP(0, value, timeoutMs);
+        arm.enableVoltageCompensation(true);
         //Controller
         //Port can be found under the USB tab of the DriverStation
         int port = 1;
         controller = new GameController(port);
         driverStation = DriverStation.getInstance();
-
     }
 }
